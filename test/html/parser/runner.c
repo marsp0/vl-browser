536baa4b8693b59837e751d12d98e907cf2ff03d
marsp0 <martin.n.spasov@gmail.com>
2025-10-27 10:40:02 +0200

    adding tests15.data

--- test/html/parser/runner.c
+++ test/html/parser/runner.c
@@ -353,6 +353,7 @@ void html_parser_test()
                                     "./test/html/parser/data/tests7.data",
                                     "./test/html/parser/data/tests8.data",
                                     "./test/html/parser/data/tests14.data",
+                                    "./test/html/parser/data/tests15.data",
                                     };
     uint32_t len = sizeof(files) / sizeof(char*);
 
